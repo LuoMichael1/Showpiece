d3fe8ba08e93a9aff930cbfa2d0abc64b89ab754
LuoMichael1 <136616479+LuoMichael1@users.noreply.github.com>
2024-06-01 16:53:13 -0400

    working on the js styling

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let root;
 let totalImages = 20;
 let iterator = totalImages;
 
-// 
+/*
 function getWidth() {
     new ResizeObserver(() => {
         window_width = window.innerWidth;
         changeWidth();
     }).observe(document.body)
 }
-
+*/
 
 // ------------- dark mode and light mode ----------------------
 function themeSetup() {
@@ -45,8 +45,7 @@ let data;
 function pictureLoad() {
     themeSetup();
     addObservers();
-    data=readJson();
-    console.log("hi" + data);
+    readJson();
 }
 
 //----------- adds images as the user scrolls down the page ----------------
@@ -54,21 +53,62 @@ let options = {
     root: null,
     rootMargin: "400px",
 };
-
+/*
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         
         if (entry.isIntersecting) {
+            // adds two new images
             for (let i=0; i<2; i++) {
 
-                let el = '<div id="img'+iterator+'" class="sketch" style="background-image: url(images/img'+iterator+'.webp);"></div>';
-                document.getElementById("gridcontent").innerHTML+=el;
-                iterator -= 1;
-                
-                console.log(data[0]);
-                
-                if (iterator == 0) {
+                if (iterator > 0) {
+                    let el = '<div id="img'+iterator+'" class="sketch" style="background-image: url(images/img'+iterator+'.webp);"></div>';
+                    document.getElementById("gridcontent").innerHTML+=el;
+                    
+                    iterator -= 1;
+                    document.getElementById("img" + iterator).style.width = "" + imgStyle[iterator][1] + "em";
+                    document.getElementById("img" + iterator).style.height = "" + imgStyle[iterator][2] + "em";
+                    document.getElementById("img" + iterator).style.margin = "" + imgStyle[iterator][3] + "em " + imgStyle[iterator][4] + "em " + imgStyle[iterator][5] + "em " + imgStyle[iterator][6] + "em ";
+                }
+                else {
                     observer.unobserve(document.getElementById("footer"));
+                    break;
+                }
+            }
+        }
+    });
+}, options);
+*/
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            for (let i = 0; i < 2; i++) {
+                if (iterator > 0) {
+                    // Create a new image element
+                    let img = document.createElement('div');
+                    img.id = "img" + iterator;
+                    img.className = "sketch";
+                    img.style.backgroundImage = "url(images/img" + iterator + ".webp)";
+                    
+                    // Append the image element to the grid content
+                    document.getElementById("gridcontent").appendChild(img);
+                    
+                    // Decrement iterator
+                    iterator--;
+
+                    try {
+                    //console.log(imgStyle[1] + "em");
+                    //Apply styles
+                    img.style.width = imgStyle[iterator][1] + "em";
+                    img.style.height = imgStyle[iterator][2] + "em";
+                    img.style.margin = imgStyle[iterator][3] + "em " + imgStyle[iterator][4] + "em " + imgStyle[iterator][5] + "em " + imgStyle[iterator][6] + "em";
+                    }
+                    catch(e) {
+                        console.log(e);
+                    }
+                    
+                } else {
                     break;
                 }
             }
@@ -81,21 +121,14 @@ function addObservers() {
 }
 
 
-
-
-
-async function readJson() {
-    const requestURL ="https://luomichael1.github.io/Showpiece/images.json";
-    const request = new Request(requestURL);
-  
-    const response = await fetch(request);
-    const data =  JSON.parse(await response.json());
-    console.log(data);
-    
-    return data;
+function readJson() {
+    fetch('https://luomichael1.github.io/Showpiece/images.json')
+        .then((response) => response.json())
+        .then((json) => applyStyle(json));
 }
 
-async function applyStyle(id) {
-    await console.log(data);
-
+let imgStyle
+function applyStyle(file) {
+    imgStyle = file; 
+    console.log(imgStyle);
 }
